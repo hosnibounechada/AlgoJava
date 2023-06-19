28e72802102096784a39a9ba08139901d49a8572
Hosni Bounechada <hosni.bounechada@gmail.com>
2023-06-19 14:15:45 +0100

    add description

--- src/main/java/org/hb/steps/Steps.java
+++ src/main/java/org/hb/steps/Steps.java
@@ -3,6 +3,24 @@ package org.hb.steps;
 import java.util.Arrays;
 
 public class Steps {
+    // --- Directions
+    // Write a function that accepts a positive number N.
+    // The function should console log a step shape
+    // with N levels using the # character.  Make sure the
+    // step has spaces on the right hand side!
+    // --- Examples
+    //   steps(2)
+    //       '# '
+    //       '##'
+    //   steps(3)
+    //       '#  '
+    //       '## '
+    //       '###'
+    //   steps(4)
+    //       '#   '
+    //       '##  '
+    //       '### '
+    //       '####'
     public void printSteps(int size) {
         for (int i = 1; i <= size; i++) {
             StringBuilder step = new StringBuilder();
